f16b7abd24223458d97f958a781e85332873802e
Yuu code <yunisafitrix@gmail.com>
2023-04-11 13:21:09 +0700

    add services footer

---
+++ src/components/footer/Services.js
@@ -0,0 +1,55 @@
+export default function Services() {
+  return (
+    <div className="container-fluid bg-success text-light d-none d-md-block">
+      <div className="container">
+      <div className="row py-4 justify-content-center align-items-center">
+        <div className="col-lg-3">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-4">
+              <img src="https://eraspace.com/icons/jaminan-produk-resmi-white.svg" alt="jaminan" className="img-fluid -mr-5"/>
+            </div>
+            <div className="col text-start">
+              <h6>Official Product Guarantee</h6>
+              <p className="lead muted" style={{fontSize: '14px'}}>100% original & safe stuff</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-4">
+              <img src="https://eraspace.com/icons/pusat-bantuan-white.svg" alt="bantuan" className="img-fluid"/>
+            </div>
+            <div className="col text-start">
+              <h6>Customer Support Center</h6>
+              <p className="lead muted" style={{fontSize: '14px'}}>Ready to help your complaints & questions</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-4">
+              <img src="https://eraspace.com/icons/cara-beli-bayar-white.svg" alt="belibayar" className="img-fluid"/>
+            </div>
+            <div className="col text-start">
+              <h6>Flexible ways to buy and pay</h6>
+              <p className="lead muted" style={{fontSize: '14px'}}>You can pay in installments until you pick up the goods at the store</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-4">
+              <img src="https://eraspace.com/icons/banyak-keuntungan-white.svg" alt="keuntungan" className="img-fluid"/>
+            </div>
+            <div className="col text-start">
+              <h6>Enjoy Many Benefits</h6>
+              <p className="lead muted" style={{fontSize: '14px'}}>Keep upgrading your MyEraspace member level</p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      </div>
+    </div>
+  );
+}
